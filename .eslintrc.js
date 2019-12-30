dae3bf383df9d4ccf34ad1e7ae95b9b8540e769f
Almighty Baka <ivanfedulov@almightybaka.com>
2019-12-30 17:21:40 +0300

    config changes and fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,19 @@
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "plugins": ["@typescript-eslint"],
-  "extends": ["plugin:@typescript-eslint/recommended"],
-  "rules": {
-    "indent": ["error", 4]
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", 'jest', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier/@typescript-eslint',
+  ],
+  rules: {
+    "indent": ["error", 4],
+    "semi": ["error", "never", { "beforeStatementContinuationChars": "never"}],
+    "delimiter": ["error", "comma"],
   }
 }
