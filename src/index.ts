173489f3efd4be91e4184f2310361a2e32bea83e
Almighty Baka <ivanfedulov@almightybaka.com>
2020-01-10 15:37:10 +0300

    added input CLI

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
+import yargs from 'yargs'
 import { processUrl } from "./lib/UrlProcessor/UrlProcessor";
 
-(async () => {
-    const result = await processUrl('http://localhost:4000/t_yWW')
-    // const result = await processUrl('https://google')
-    console.log(result)
-    return
-})()
+// (async () => {
+//     const result = await processUrl('http://localhost:4000/t_yWW')
+//     // const result = await processUrl('https://google')
+//     console.log(result)
+//     return
+// })()
+
+yargs.command('set [url]', 'Get a shortened version of URL'
+    , (yargs) => {
+        yargs.positional('url', {
+            describe: 'URL to be shortened',
+            type: 'string',
+        })
+    }, async (args) => {
+        const result = await processUrl(args.url as string)
+        console.log(result)
+    })
+.demandOption(['url'], 'Please provide an URL')
+
+yargs.command('get [url]', 'Get original version of URL'
+    , (yargs) => {
+        yargs.positional('url', {
+            describe: 'URL to get original of',
+            type: 'string',
+        })
+    }, async (args) => {
+        const result = await processUrl(args.url as string)
+        console.log(result)
+    })
+.demandOption(['url'], 'Please provide an URL')
+
+yargs.help()
+.argv
